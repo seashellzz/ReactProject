9c7f5e01a4afe193c72e672ecad27f2e0960628f
seashellzz <xzzhannah@gmail.com>
2024-12-20 13:49:39 -0800

    added column bar in student list page

--- src/pages/StudentList.jsx
+++ src/pages/StudentList.jsx
@@ -1,8 +1,68 @@
 import React from "react";
+import studentlist from "../testdata/studentlist.json";
+import { useTable } from "react-table";
 export default function StudentList() {
+
+  const data = React.useMemo(() => studentlist, []);
+  const columns = React.useMemo(() => [
+    {
+      Header: "First Name",
+      accessor: "first_name",
+    },
+    {
+      Header: "Last Name",
+      accessor: "last_name",
+    },
+    
+    {
+      Header: "Grade",
+      accessor: "grade",
+    },
+    {
+      Header: "Gender",
+      accessor: "gender",
+    },
+    {
+      Header: "Date of Birth",
+      accessor: "dob",
+    },
+    {
+      Header: "Institution",
+      accessor: "institution",
+    },
+    
+    {
+      Header: "Guardian Email",
+      accessor: "primary_guardian_email",
+    },
+
+  ], []
+);
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data});
+
   return (
     <div className="student-list" >
-        part 1
+        
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
